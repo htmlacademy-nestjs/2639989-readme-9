597ce66de73dfd6c0080adde5dddc2ea0cc8e5a0
Gekkonik <cyberninja2007@duck.com>
2025-05-21 17:18:30 +0300

    Зарегистрировал маршрут для регистрации

--- project/libs/account/authentication/src/authentication-module/authentication.controller.ts
+++ project/libs/account/authentication/src/authentication-module/authentication.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Post} from '@nestjs/common';
 import {AuthenticationService} from "./authentication.service";
+import {CreateUserDto} from "../dto/create-user.dto";
 
 @Controller('auth')
 export class AuthenticationController {
   constructor(private readonly authenticationService: AuthenticationService) {}
+
+  @Post('register')
+  public async create(@Body() dto: CreateUserDto) {
+    const newUser = await this.authenticationService.register(dto);
+    return newUser.toPOJO();
+  }
 }
