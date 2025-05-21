dc6549ac5297f3002dd186b7bf227b7f028ddcba
Gekkonik <cyberninja2007@duck.com>
2025-05-21 17:11:58 +0300

    Добавил метод для проверки пользователя и его получения

--- project/libs/account/authentication/src/authentication-module/authentication.constant.ts
+++ project/libs/account/authentication/src/authentication-module/authentication.constant.ts
@@ -1,1 +1,3 @@
 export const AUTH_USER_EXISTS = 'Пользователь с данным email уже существует!';
+export const AUTH_USER_NOT_FOUND = 'Пользователь не найден';
+export const AUTH_USER_PASSWORD_WRONG = 'Пароль пользователя не правильный';
