f301e9ce1816014169a1bc5ac87eb22af60213ce
Gekkonik <cyberninja2007@duck.com>
2025-05-31 14:33:48 +0300

    Добавил валидацию с помощью class-validator

--- project/libs/account/config/src/account.config.constant.ts
+++ project/libs/account/config/src/account.config.constant.ts
@@ -1,4 +1,3 @@
 export const DEFAULT_PORT = 3000;
 export const ENVIRONMENTS = ['development', 'production', 'stage'] as const;
 export const ENV_USERS_FILE_PATH = 'apps/account/account.env';
-export const DEFAULT_MONGO_PORT = 27017;
