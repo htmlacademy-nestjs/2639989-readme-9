50748f5c661165b63db2acb0cdf1952dca0f2c8b
Gekkonik <cyberninja2007@duck.com>
2025-05-31 14:13:58 +0300

    Добавил загрузку конфигурации mongo

--- project/libs/account/config/src/account-config.module.ts
+++ project/libs/account/config/src/account-config.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import {ENV_USERS_FILE_PATH} from "./account.config.constant";
 import applicationConfig from "./configurations/app.config";
+import mongoConfig from './configurations/mongo.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       cache: true,
-      load: [applicationConfig],
+      load: [applicationConfig, mongoConfig],
       envFilePath: ENV_USERS_FILE_PATH
     }),
   ]
