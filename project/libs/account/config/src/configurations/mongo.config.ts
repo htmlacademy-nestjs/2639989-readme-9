9cf0595736e46f2a110cb44786eaea987c3e58af
Gekkonik <cyberninja2007@duck.com>
2025-05-31 14:37:32 +0300

    Обновил получение и валидацию настроек на class-validator

--- project/libs/account/config/src/configurations/mongo.config.ts
+++ project/libs/account/config/src/configurations/mongo.config.ts
@@ -1,44 +1,22 @@
-import { registerAs } from '@nestjs/config';
-import * as Joi from 'joi';
-import {DEFAULT_MONGO_PORT} from "../account.config.constant";
+import {ConfigType, registerAs} from '@nestjs/config';
+import {DEFAULT_MONGO_PORT} from "./mongodb/mongo.constant";
+import {MongoConfiguration} from "./mongodb/mongo.env";
+import {plainToClass} from "class-transformer";
 
-export interface MongoConfig {
-  host: string;
-  name: string;
-  port: number;
-  user: string;
-  password: string;
-  authBase: string;
-}
-
-const dbValidationSchema = Joi.object({
-  host: Joi.string().hostname().required(),
-  port: Joi.number().port().default(DEFAULT_MONGO_PORT),
-  name: Joi.string().required(),
-  user: Joi.string().required(),
-  password: Joi.string().required(),
-  authBase: Joi.string().required(),
-});
-
-function validateMongoConfig(config: MongoConfig): void {
-  const { error } = dbValidationSchema.validate(config, { abortEarly: true });
-  if (error) {
-    throw new Error(`[Ошибка валидации конфигурации MongoDB]: ${error.message}`);
-  }
-}
-
-function getDbConfig(): MongoConfig {
-  const config: MongoConfig = {
+async function getDbConfig(): Promise<MongoConfiguration> {
+  const config = plainToClass(MongoConfiguration, {
     host: process.env.MONGO_HOST,
     name: process.env.MONGO_DB,
     port: parseInt(process.env.MONGO_PORT ?? `${DEFAULT_MONGO_PORT}`, 10),
     user: process.env.MONGO_USER,
     password: process.env.MONGO_PASSWORD,
     authBase: process.env.MONGO_AUTH_BASE,
-  };
+  });
 
-  validateMongoConfig(config);
+  await config.validate();
   return config;
 }
 
-export default registerAs('db', getDbConfig);
+export default registerAs('db', async (): Promise<ConfigType<typeof getDbConfig>> => {
+  return getDbConfig();
+});
