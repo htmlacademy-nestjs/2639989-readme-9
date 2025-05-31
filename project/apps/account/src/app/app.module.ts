b4442877a4d3d664d24349457892fe19c8546be8
Gekkonik <cyberninja2007@duck.com>
2025-05-31 15:11:40 +0300

    Подключил mongoDB

--- project/apps/account/src/app/app.module.ts
+++ project/apps/account/src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { Module } from '@nestjs/common';
 import {BlogUserModule} from "@project/blog-user";
 import {AuthenticationModule} from "@project/authentication";
-import {AccountConfigModule} from "@project/account-config";
+import {AccountConfigModule, getMongooseOptions} from "@project/account-config";
+import {MongooseModule} from "@nestjs/mongoose";
 
 @Module({
   imports: [
     BlogUserModule,
     AuthenticationModule,
-    AccountConfigModule
+    AccountConfigModule,
+    MongooseModule.forRootAsync(
+      getMongooseOptions()
+    )
   ],
   controllers: [],
   providers: [],
