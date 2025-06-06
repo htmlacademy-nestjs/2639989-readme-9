875867c4a20f0f878dbef4e8769009d8fc9db811
Gekkonik <cyberninja2007@duck.com>
2025-06-06 15:46:51 +0300

    Добавил интерфейсы

---
+++ project/libs/shared/core/src/lib/types/post.interface.ts
@@ -0,0 +1,21 @@
+import {Tag} from "./tag.interface";
+import {Like} from "./like.interface";
+
+export interface Post {
+  id: string;
+  userId: string;
+  type: PostType;
+  payload: Record<string, any>;
+  createdAt: Date;
+  publishedAt: Date;
+  status: PostStatus;
+  isRepost: boolean;
+  originalPostId?: string;
+  reposts?: Post[];
+  tags?: Tag[];
+  likes?: Like[];
+  comments?: Comment[];
+}
+
+export type PostType = 'TEXT' | 'VIDEO' | 'QUOTE' | 'PHOTO' | 'LINK';
+export type PostStatus = 'DRAFT' | 'PUBLISHED';
